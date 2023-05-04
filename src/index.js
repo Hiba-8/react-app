1af664a557f5e6e1c6a4e5693d4e2eba3a490535
Hiba.KH <77845394+Hiba-Khouzai@users.noreply.github.com>
2023-05-04 18:11:49 +0200

    added another new line

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // a new line
-// Another new line
+// Another new line 22
 reportWebVitals();
